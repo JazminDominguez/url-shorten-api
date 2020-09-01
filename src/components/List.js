3fb5a4dc22c4d1568bd4c97245aab72e8a71841b
Jaz Dominguez <j.dgez.p@gmail.com>
2020-08-31 19:59:50 -0500

    Add html for listed elements. Add copy to clipboard function. Add basic styles fot shorted links

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
+import { Flex, Box } from "rebass";
 
 const List = () => {
   const links = useSelector((state) => state);
+  const [buttonId, setButtonId] = useState("");
+  const shortLinkRef = useRef(null);
+
+  const handleClick = (e) => {
+    setButtonId(e.target.id);
+    navigator.clipboard.writeText(`https://rel.ink/${e.target.id}`);
+  };
 
   const linksList = links ? (
     links.map((link) => {
       return (
-        <div className="collection-item" key={link.hashid}>
-          <span>{link.url}</span>
-          <span>https://rel.ink/{link.hashid}</span>
-        </div>
+        <Flex
+          className="collection-links"
+          flexDirection={["column", "row"]}
+          marginY={[3]}
+          key={link.hashid}
+        >
+          <Box className="original-link">{link.url}</Box>
+          <Box className="cropped-link">https://rel.ink/{link.hashid}</Box>
+          <Box id={link.hashid}>
+            <button
+              id={link.hashid}
+              value={`https://rel.ink/${link.hashid}`}
+              ref={shortLinkRef}
+              className={
+                "copy-button " + (buttonId === link.hashid ? "copied" : "")
+              }
+              onClick={handleClick}
+            >
+              {buttonId === link.hashid ? "Copied!" : "Copy"}
+            </button>
+          </Box>
+        </Flex>
       );
     })
   ) : (
     <p>you have no links left</p>
   );
-  return <div>{linksList}</div>;
+  return <section id="list">{linksList}</section>;
 };
 
 List.propTypes = {
