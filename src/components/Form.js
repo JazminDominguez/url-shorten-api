0b36475a2e3ffe1f476949c1ac6f665f3f5b4e75
Jaz Dominguez <j.dgez.p@gmail.com>
2020-09-14 19:14:06 -0500

    Fix link list styles. Modify body color

--- src/components/Form.js
+++ src/components/Form.js
@@ -23,45 +23,47 @@ const Form = () => {
   };
 
   return (
-    <form className="form-container">
-      <Flex
-        flexWrap="wrap"
-        flexDirection={["column", "row"]}
-        alignItems={"center"}
-        padding={[3, 4]}
-      >
-        <Box width={[1, 3 / 4, 4 / 5]} padding={[1]}>
-          <Flex flexDirection="column">
-            <input
-              type="text"
-              className={error === true ? "error error--message" : ""}
-              placeholder="Shorten a link here..."
-              onChange={handleChange}
-              value={originalLink}
-            />
-            {error ? (
-              <span className="error">please add a link</span>
-            ) : (
-              <span />
-            )}
-          </Flex>
-        </Box>
-        <Box
-          width={[1, 1 / 4, 1 / 5]}
-          textAlign="center"
-          className={error ? "button-container" : ""}
-          padding={[1]}
+    <section id="form">
+      <form className="form-container">
+        <Flex
+          flexWrap="wrap"
+          flexDirection={["column", "row"]}
+          alignItems={"center"}
+          padding={[3, 4]}
         >
-          <button
-            className="button_shorten_it"
-            type="submit"
-            onClick={handleSubmit}
+          <Box width={[1, 3 / 4, 4 / 5]} padding={[1]}>
+            <Flex flexDirection="column">
+              <input
+                type="text"
+                className={error === true ? "error error--message" : ""}
+                placeholder="Shorten a link here..."
+                onChange={handleChange}
+                value={originalLink}
+              />
+              {error ? (
+                <span className="error">please add a link</span>
+              ) : (
+                <span />
+              )}
+            </Flex>
+          </Box>
+          <Box
+            width={[1, 1 / 4, 1 / 5]}
+            textAlign="center"
+            className={error ? "button-container" : ""}
+            padding={[1]}
           >
-            Shorten It!
-          </button>
-        </Box>
-      </Flex>
-    </form>
+            <button
+              className="button_shorten_it"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Shorten It!
+            </button>
+          </Box>
+        </Flex>
+      </form>
+    </section>
   );
 };
 
